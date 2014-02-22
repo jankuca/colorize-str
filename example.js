dd873ea8badd24ee8f55f5fe1c63cc52680324c9
alfred <alfred@klandestino.se>
2014-02-22 12:01:48 +0000

    nicer example

--- example.js
+++ example.js
@@ -1,6 +1,14 @@
 var
-	printc = require('./index.js');
+	colStr = require('./index.js');
 
-console.log(printc('{#fff} Hej{#700} du{#a00} du{#300} du'));
+var
+	rows = 24,
+	str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-console.log(printc('ap{rgb(200, 60, 60)}a'));
+for (var y = 0; y < rows; y++) {
+	for (var x = 0; x < str.length; x++) {
+		process.stdout.write(colStr('{rgb(' + Math.round((x / (str.length - 1)) * 255) + ', ' + Math.round((y / (rows - 1)) * 255) + ', ' + (255 - Math.round(((x + y) / ((rows + str.length) - 2)) * 255)) + ')}' + str.substr(x, 1)));
+	}
+	process.stdout.write("\n");
+}
+
