fc88d413f1e87da4e8abbaf569a33b52e19f79c1
alfred <alfred@klandestino.se>
2014-02-22 12:04:53 +0000

    easier to read example

--- example.js
+++ example.js
@@ -7,7 +7,10 @@ var
 
 for (var y = 0; y < rows; y++) {
 	for (var x = 0; x < str.length; x++) {
-		process.stdout.write(colStr('{rgb(' + Math.round((x / (str.length - 1)) * 255) + ', ' + Math.round((y / (rows - 1)) * 255) + ', ' + (255 - Math.round(((x + y) / ((rows + str.length) - 2)) * 255)) + ')}' + str.substr(x, 1)));
+		var r = Math.round((x / (str.length - 1)) * 255);
+		var g = Math.round((y / (rows - 1)) * 255);
+		var b = (255 - Math.round(((x + y) / ((rows + str.length) - 2)) * 255));
+		process.stdout.write(colStr('{rgb(' + r + ', ' + g + ', ' + b + ')}' + str.substr(x, 1)));
 	}
 	process.stdout.write("\n");
 }
